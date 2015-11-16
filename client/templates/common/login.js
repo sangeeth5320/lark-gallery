42aa370e1eeaaa52cd497d8148560ea295125caf
sangeeth <sangeethk@devois.com>
2015-11-16 16:49:46 +0530

    Delete functionality included ..

--- client/templates/common/login.js
+++ client/templates/common/login.js
@@ -10,19 +10,37 @@ Template.login.events({
         Meteor.loginWithPassword(email, password, function (err) {
             if (err) {
                 console.log("not authorized");
+                toastr.error('not authorized');
             }
             else {
+                toastr.success("logged in");
                 console.log("logged in");
                 Router.go('/');
             }
-       });
+
+        });
+
+
         return false;
     }
 });
 
 
 Template.logout.events({
-   
-    });
+    "click .logout": function (event) {
+        Meteor.logout(email, password, function (err) {
+            if (err) {
+                toastr.success("Problem in logging out");
+                console.log("Problem in logging out");
+
+            }
+            else {
+                toastr.success("logged out");
+                console.log("logged out");
+            }
+        });
+    }
+ });
+
 
 
